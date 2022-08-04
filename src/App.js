6a5aa826ba943c0eca9120d4c8efe24cc292b5d1
Shahbaaz <shahbaazc@gmail.com>
2022-08-04 12:54:06 +0530

    translation code added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+import { language } from "./Services/lang-list";
+
+
 
 function App() {
+  const { t } = useTranslation();
+
+  const releaseDate = new Date('2022-03-07');
+  const timeDifference = new Date() - releaseDate;
+  const number_of_days = Math.floor(timeDifference / (1000*60*60*24))
+
+  const loadingMarkup = (
+    <h2>Loading...</h2>
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Suspense tag wont allow to reflect code
+    <Suspense fallback={loadingMarkup}>
+      <div className="App">
+        <header className="App-header">
+         <ul>
+         {language.map(item => (
+              <li onClick={() => {i18next.changeLanguage(item.code)}}>{item.name}</li>
+            ))} 
+          </ul>
+          <h1>{t("welcome_to_react")}</h1>
+          {/* change dynamic number */}
+          <p>{t('days_since_release', {number_of_days})}</p>
+        </header>
+      </div>
+    </Suspense>
   );
 }
 
