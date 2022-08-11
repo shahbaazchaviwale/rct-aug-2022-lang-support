f2a98198b2ccc3941d84edf2d76b365654ff16e2
Shahbaaz <shahbaazc@gmail.com>
2022-08-11 13:15:01 +0530

    local language support

---
+++ src/Services/lang-list.js
@@ -0,0 +1,20 @@
+export const language = [
+    {
+      code: 'en',
+      name: 'English',
+      type: 'English',
+      country_code: 'gb'
+    },
+    {
+      code: 'fr',
+      name: 'Français',
+      type: 'French',
+      country_code: 'fr'
+    },
+    {
+      code: 'jp',
+      name: 'フランス語',
+      type: 'Japnese',
+      country_code: 'jp'
+    },
+  ];
